fc00c45e52bfdaf19e07d5ee7f9c7d5a0e09d673
Jawad786-wa <ajawadahmad29@gmail.com>
2025-07-08 14:12:25 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# Muhammad Jawad - SU-22-01-002-032
+
+def add(a, b):
+    return a + b
+
+def main():
+    print("Simple Python Calculator")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Sum:", add(num1, num2))
+
+if __name__ == "__main__":
+    main()
